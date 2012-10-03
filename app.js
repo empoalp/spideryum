18e6b1a1d2ebfca18a579154fd4a8d83a7bf7495
David Garcia <davidg@tid.es>
2012-10-03 18:13:09 +0200

    Some work on this

--- app.js
+++ app.js
@@ -4,9 +4,11 @@
  */
 
 var express = require('express')
+  , fs = require('fs')
   , routes = require('./routes')
   , user = require('./routes/user')
   , http = require('http')
+  , spideryumPath = process.env.SPIDERYUM_PATH
   , path = require('path');
 
 var app = express();
@@ -20,6 +22,7 @@ app.configure(function(){
   app.use(express.bodyParser());
   app.use(express.methodOverride());
   app.use(app.router);
+  app.use(express.static(spideryumPath));
   app.use(express.static(path.join(__dirname, 'public')));
 });
 
@@ -27,8 +30,34 @@ app.configure('development', function(){
   app.use(express.errorHandler());
 });
 
-app.get('/', routes.index);
-app.get('/users', user.list);
+app.get(/^\/(.*)/, function(req, res, next){
+  var pathString, pathArray;
+  if (req.query.type === 'static') {
+    next();
+  } else {
+      pathString = req.params[0] || '';
+      pathArray = pathString.split('/');    
+      console.log(pathString);
+      console.log(path.join(spideryumPath, pathString));
+      fs.stat(path.join(spideryumPath, pathString), function(err, stats) {
+          if (err || stats.isFile()) {
+            next();
+          } else if (stats.isDirectory) {
+              fs.readdir(path.join(spideryumPath, pathString), function(err, files) {
+                  if (err) {
+                  } else {
+                      files.sort();
+                      res.render('index', { path: pathString, files: files });
+                  }              
+              });
+          }
+      });
+  }
+});
+
+app.get('/upload', function(req, res) {
+  res.render('upload');
+});
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log("Express server listening on port " + app.get('port'));
